e85366937ad25d47bb7c2dc876d6111a76d5b9af
arjunn881 <arjunn881@gmail.com>
2023-06-20 22:43:27 +0530

    20/06 22:44

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
 
 import '../src/App.css'; 
+import { About } from './component/About/About';
+import { Contact } from './component/Contact/Contact';
+import { Experience } from './component/Experience/Experience';
+import { Footer } from './component/Footer/Footer';
+import { Header } from './component/Header/Header';
+import { Nav } from './component/Nav/Nav';
+import { Portfolio } from './component/Portfolio/Portfolio';
+import { Services } from './component/Services/Services';
+import { Testimonials } from './component/Testimonials/Testimonials';
 
 function App() {
   return (
     <div className="App">
-      Portfolio...........
+      <Header />
+      <Nav/>
+      <About/>
+      <Experience/>
+      <Services/>
+      <Portfolio/>
+      <Testimonials/>
+      <Contact/>
+      <Footer/>
     </div>
   );
 }
