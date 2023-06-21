890c6324b6e8252750e532a383ac050301dc2bfa
arjunn881 <arjunn881@gmail.com>
2023-06-21 17:31:57 +0530

    21/06 17:32

---
+++ src/component/Header/CTA.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './Header.css';
+
+export const CTA = () => {
+  return (
+    <div className='cta'>
+        <a href="" download  className='btn'>Download Resume</a>
+        <a href="#contact" className='btn  btn-primary'>Let's Talk</a>
+    </div>
+  )
+}
--- src/component/Header/Header.jsx
+++ src/component/Header/Header.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
 import '../Header/Header.css';
+import { CTA } from './CTA';
+import { HeaderSocials } from './HeaderSocials';
 
 
 export const Header = () => {
   return (
-    <div>Header</div>
+   <header>
+    <div className="container header__container">
+      <h5>Hello I'm</h5>
+      <h1>Arjuna Nayak</h1>
+      <h5 className="text-light">Fullstack Developer</h5>
+      <CTA/>
+      <HeaderSocials/>
+
+      <div className="me">
+        <img src="" alt="" />
+      </div>
+
+      <a href="#contact" className='scroll__down'>Scroll Down</a>
+    </div>
+   </header>
   )
 }
