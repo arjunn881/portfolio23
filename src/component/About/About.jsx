b79a5bc4ab54818b6d0ee4d65bad4e11d13d9d0c
arjunn881 <arjunn881@gmail.com>
2023-06-23 00:27:45 +0530

    23/06 00:27

--- src/component/About/About.jsx
+++ src/component/About/About.jsx
@@ -1,12 +1,9 @@
 import React from "react";
 import "../About/About.css";
-import Me from '../../asset/Arjun.png'
-import {FaAward} from 'react-icons/fa'
-import {FiUsers} from 'react-icons/fi'
-import {VscFolderLibrary} from 'react-icons/vsc'
-
-
-
+import Me from "../../asset/Arjun.png";
+import { FaAward } from "react-icons/fa";
+import { FiUsers } from "react-icons/fi";
+import { VscFolderLibrary } from "react-icons/vsc";
 
 export const About = () => {
   return (
@@ -16,31 +13,37 @@ export const About = () => {
 
       <div className="container about__container">
         <div className="about__me">
-          <div className="about_me-image">
+          <div className="about__me-image">
             <img src={Me} alt="" />
           </div>
         </div>
         <div className="about__content">
-          <div className="abot__cards">
+          <div className="about__cards">
             <article className="about__card">
-              <FaAward className="about__icon"/>
+              <FaAward className="about__icon" />
               <h5>Experience</h5>
               <small>1+ Years Working</small>
             </article>
 
             <article className="about__card">
-              <FiUsers className="about__icon"/>
+              <FiUsers className="about__icon" />
               <h5>Experience</h5>
               <small>40+ Worldwide</small>
             </article>
 
-
             <article className="about__card">
-              <VscFolderLibrary className="about__icon"/>
+              <VscFolderLibrary className="about__icon" />
               <h5>Experience</h5>
               <small>30+ Completed</small>
             </article>
           </div>
+          <p>
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum has been the industry's standard dummy text
+            ever since the 1500s, when an unknown printer took a galley of type
+            and scrambled it to make a type specimen book.
+          </p>
+          <a href="#contact" className="btn btn-primary "> Let's Talk</a>
         </div>
       </div>
     </section>
