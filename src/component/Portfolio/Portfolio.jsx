e5065c398bac610fe6216dc956bf512754c19d85
arjunn881 <arjunn881@gmail.com>
2023-06-29 13:32:20 +0530

    29/06  13:32

--- src/component/Portfolio/Portfolio.jsx
+++ src/component/Portfolio/Portfolio.jsx
@@ -2,6 +2,20 @@ import React from 'react'
 
 export const Portfolio = () => {
   return (
-    <section id='portfolio'>Portfolio</section>
+    <section id='portfolio'>
+      <h5>My Recent Work</h5>
+      <h2>Portfolio</h2>
+
+      <div className="container portfolio__container">
+        <article className='portfolio__item'>
+                <div className="portfolio__item-image">
+                  <h3>This is a portfolio item title</h3>
+                  <a href="http://github.com/arjunn881" className='btn'>Github</a>
+                  <a href="http://github.com/arjunn881" target='_blank' className='btn btn-primary'>Live Demo</a>
+                  
+                </div>
+        </article>
+      </div>
+    </section>
   )
 }
