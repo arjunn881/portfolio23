4b401a71dda8b9ddbbd47e5a24f8a27c3f44974d
arjunn881 <arjunn881@gmail.com>
2023-07-02 23:37:14 +0530

    02/07 23:37

--- src/component/Contact/Contact.jsx
+++ src/component/Contact/Contact.jsx
@@ -1,8 +1,30 @@
 import React from 'react'
 import '../Contact/Contact.css';
+import { MdOutlineEmail } from 'react-icons/md'
 
 export const Contact = () => {
   return (
-    <section id='contact'>Contact</section>
+    <section id='contact'>
+      <h5>Get In Touch</h5>
+      <h2>Contact Me</h2>
+
+
+      <div className="container contact__container">
+        <div className="contact__options">
+          <article className="contact__option">
+            <MdOutlineEmail/>
+            <h4>Email</h4>
+            <h5>arjunn881@gmail.com</h5>
+            <a href="arjunn881@gmail.com">
+              Send a message
+            </a>
+          </article>
+        </div>
+
+        <form action="">
+
+        </form>
+      </div>
+    </section>
   )
 }
