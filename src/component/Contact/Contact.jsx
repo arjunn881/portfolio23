a749e578fb8a905009173a122680bc660e7b5abe
arjunn881 <arjunn881@gmail.com>
2023-07-02 23:52:07 +0530

    02/07 23:51

--- src/component/Contact/Contact.jsx
+++ src/component/Contact/Contact.jsx
@@ -1,6 +1,10 @@
 import React from 'react'
 import '../Contact/Contact.css';
 import { MdOutlineEmail } from 'react-icons/md'
+import { RiMessengerLine } from 'react-icons/ri'
+import { BsWhatsapp } from 'react-icons/bs';
+
+
 
 export const Contact = () => {
   return (
@@ -13,6 +17,25 @@ export const Contact = () => {
         <div className="contact__options">
           <article className="contact__option">
             <MdOutlineEmail/>
+            <h4>Email</h4>
+            <h5>arjunn881@gmail.com</h5>
+            <a href="arjunn881@gmail.com">
+              Send a message
+            </a>
+          </article>
+
+          <article className="contact__option">
+            <RiMessengerLine/>
+            <h4>Email</h4>
+            <h5>arjunn881@gmail.com</h5>
+            <a href="arjunn881@gmail.com">
+              Send a message
+            </a>
+          </article>
+
+
+          <article className="contact__option">
+            <BsWhatsapp/>
             <h4>Email</h4>
             <h5>arjunn881@gmail.com</h5>
             <a href="arjunn881@gmail.com">
