63ec96aa358b058356c61840310a8932fc8f94cf
arjunn881 <arjunn881@gmail.com>
2023-08-13 23:31:36 +0530

    13/08 23:31

--- src/component/Footer/Footer.jsx
+++ src/component/Footer/Footer.jsx
@@ -1,7 +1,43 @@
-import React from 'react'
+import React from "react";
 
 export const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <footer>
+      <a href="#" className="footer__logo">
+        ARJUNA
+      </a>
+
+      <ul className="permalinks">
+        <li>
+          <a href="#">Home</a>
+        </li>
+
+        <li>
+          <a href="#">Home</a>
+        </li>
+
+        <li>
+          <a href="#">Home</a>
+        </li>
+
+        <li>
+          <a href="#">Home</a>
+        </li>
+
+        <li>
+          <a href="#">Home</a>
+        </li>
+
+        <li>
+          <a href="#">Home</a>
+        </li>
+      </ul>
+
+      <div className="footer__socials">
+        <a href=""></a>
+        <a href=""></a>
+        <a href=""></a>
+      </div>
+    </footer>
+  );
+};
