3e16519158b66c6f708a724d2758903c8b357d31
arjunn881 <arjunn881@gmail.com>
2023-11-22 16:08:24 +0530

    updated

--- src/component/Header/CTA.jsx
+++ src/component/Header/CTA.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import './Header.css';
+import Resume from '../../asset/Resume.pdf'
 
 export const CTA = () => {
   return (
     <div className='cta'>
-        <a href="" download  className='btn'>Download Resume</a>
+        <a href={Resume} download = "Resume"  className='btn'>Download Resume</a>
         <a href="#contact" className='btn  btn-primary'>Let's Talk</a>
     </div>
   )
